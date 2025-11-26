86d5962cbf54d731ac6850a55e03f7aee37a6aae
MrRajput0240 <rajshekhar0240@gmail.com>
2025-11-26 23:46:40 +0530

    day9

---
+++ Neon_Number.java
@@ -0,0 +1,20 @@
+package  com.number_program;
+public class Neon_Number{ 
+
+    public static int square(int num){
+        return num *num ;
+    }
+    public static void main(String[] args) {
+        int square =square(num),sum=0;
+        while (square !=0){
+            sum =(square%10);
+            Square /=10 ;
+        }
+        return num==sum ?"a NeonNumber":"not a Neon number ";
+         }
+         Public static void main (String[]args){
+            
+         }
+
+    }
+}

3890fa84617294311ceeb86fbc7536c9427652e2
MrRajput0240 <rajshekhar0240@gmail.com>
2025-11-27 22:02:03 +0530

    day10

--- Neon_Number.java
+++ Neon_Number.java
@@ -1,4 +1,7 @@
 package  com.number_program;
+
+import java.util.Scanner;
+
 public class Neon_Number{ 
 
     public static int square(int num){
@@ -8,11 +11,17 @@ public class Neon_Number{
         int square =square(num),sum=0;
         while (square !=0){
             sum =(square%10);
-            Square /=10 ;
+            square /=10 ;
         }
-        return num==sum ?"a NeonNumber":"not a Neon number ";
+        return num == sum?"a NeonNumber":"not a Neon number ";
          }
          Public static void main (String[]args){
+         scanner sc=new Scanner(system.in);
+         system.out.println("provide the number :");
+         int num= sc.nextInt();
+         String result = checkneon (num);
+         system.out.println("num" is +result)
+
             
          }
 

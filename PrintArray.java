d748be4300534dae84629e5a5caa0df1484782ff
MrRajput0240 <rajshekhar0240@gmail.com>
2025-12-07 22:27:47 +0530

    day19

---
+++ PrintArray.java
@@ -0,0 +1,12 @@
+public  class  PrintArray {
+    public static void main(String[] args) {
+        int[] number ={10,20,30,40,50};
+        System.out.println("Array element:");
+        for(int i=0;i< number.length; i++) {
+        
+        System.out.println(number[i]);
+
+        }
+        
+    }
+}

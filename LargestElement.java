2daca4ebff0d9bbb7f2dd9964b4a0a12d1e0d7c4
MrRajput0240 <rajshekhar0240@gmail.com>
2025-12-09 00:17:15 +0530

    day21

---
+++ LargestElement.java
@@ -0,0 +1,23 @@
+package com.array_program;
+
+public class LargestElement {
+
+    // Method to find the maximum element in the array
+    public static int maxElement(int[] a) {
+        int max = a[0]; // assume first element is max
+
+        // compare each element with current max
+        for (int i = 1; i < a.length; i++) {
+            if (max < a[i])
+                max = a[i];
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {6, 7, 8, 4, 3, 2, 10, 0};
+
+        // Correct way to call the method and print result
+        System.out.println("Largest element is: " + maxElement(a));
+    }
+}
